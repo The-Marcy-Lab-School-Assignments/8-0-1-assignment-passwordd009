349c6843d4b23ec4ec26db0c03f297b07cd7a98e
benspector-mls <ben@marcylabschool.org>
2024-04-10 12:53:46 -0400

    code graded: 7/12 tests passing

--- server/index.js
+++ server/index.js
@@ -4,14 +4,19 @@ const app = express();
 const path = require('path');
 
 const logRoutes = (req, res, next) => {
-    const time = new Date().toLocaleString();
-    console.log(`${req.method}: ${req.originalUrl} - ${time}`);
-    next(); 
-  };
+  const time = new Date().toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next();
+};
 
+/* FEEDBACK: You need to create a Vite React project, build it
+and then construct a path to the dist folder!
+ */
 const pathToDistFolder = path.join(__dirname, '..', 'path', 'to', 'frontend', 'dist');
 
 const serveStatic = express.static(pathToDistFolder);
 
 app.use(logRoutes)
 app.use(serveStatic);
+
+/* FEEDBACK: Don't forget to have the app listen! */
